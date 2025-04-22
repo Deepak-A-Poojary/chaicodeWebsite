8eafc7d824cbf0dfa400a16fd7c940492ebcd735
Deepak A Poojary <deepaklpoojary@gmail.com>
2025-04-22 14:30:35 +0530

    refactor: created indexjs in customIcon folder to make import statement cleaner in navbar

---
+++ src/components/customIcon/index.js
@@ -0,0 +1,4 @@
+export { default as CohortIcon } from "./CohortIcon";
+export { default as BookIcon } from "./BookIcon";
+export { default as DocsIcon } from "./DocsIcon";
+export { default as ReviewIcon } from "./ReviewIcon";
